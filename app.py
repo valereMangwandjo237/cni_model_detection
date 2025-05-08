4cc481355877515967d4f3465a833139eead58c9
valere codeur <78545084+valereMangwandjo237@users.noreply.github.com>
2025-05-08 13:36:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -140,7 +140,7 @@ with col2:
                 # Résultat avec mise en forme conditionnelle
                 if predicted_class == "CNI" or predicted_class == "recepisse" or predicted_class == "passport":
                     st.markdown(f"<h2 style='color: #1abc9c;'>📋 {predicted_class}</h2>", unsafe_allow_html=True)
-                    text_cni = extraction(temp_path)
+                    text_cni = extraction(uploaded_file)
                     with st.expander("📝 Texte extrait de la CNI"):
                         st.text(text)
                 else:
