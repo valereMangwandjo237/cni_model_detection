0720a31698f4c3dc6f1f70aef911fe731a7443f8
valere codeur <78545084+valereMangwandjo237@users.noreply.github.com>
2025-05-22 12:35:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -138,11 +138,10 @@ with col2:
                 # Résultat avec mise en forme conditionnelle
                 if predicted_class == "CNI" or predicted_class == "recepisse" or predicted_class == "passport":
                     st.markdown(f"<h2 style='color: #1abc9c;'>📋 {predicted_class}</h2>", unsafe_allow_html=True)
-                    text_cni = extraction_ocr(temp_path)
-                    with st.expander("📝 Texte extrait de la CNI"):
-                        st.write(f"**Nom:** {text_cni['nom']}")
-                        st.write(f"**Prenom:** {text_cni['prenom']}")
-                        st.write(f"**Texte brut OCR:** {text_cni['texte_brut']}")
+                
+                    st.write(f"**Nom:** {text_cni['nom']}")
+                    st.write(f"**Prenom:** {text_cni['prenom']}")
+                    st.write(f"**Texte brut OCR:** {text_cni['texte_brut']}")
                 else:
                     st.markdown(f"<h2 style='color: #f39c12;'>❌ {predicted_class}</h2>", unsafe_allow_html=True)
                      
